27fb5f18ee92fb644bdc1ed2be29f85e11aa4114
science-math-guy <adumont@mailbox.org>
2021-03-20 00:02:33 +0100

    changes

--- server.js
+++ server.js
@@ -3,11 +3,28 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.set('view engine', 'ejs');
+
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.sendFile(`${__dirname}/public/eleve.html`);
+    res.redirect('/eleve');
+});
+
+app.get('/eleve',(req,res)=>{
+    res.render('eleve');
+});
+
+app.get('/auth',(req,res)=>{
+    res.render('finish');
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.redirect('/auth');
 })
 
 app.listen(PORT,()=>{
